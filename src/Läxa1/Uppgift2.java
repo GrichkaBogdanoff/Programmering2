c27073a1cdf6030d9c9fd6c30ed832c3c239d73f
GrichkaBogdanoff <42673891+GrichkaBogdanoff@users.noreply.github.com>
2019-09-04 09:26:33 +0200

    Create Uppgift2.java

---
+++ src/Läxa1/Uppgift2.java
@@ -0,0 +1,21 @@
+package Läxa1;
+
+import java.util.Scanner;
+
+public class Uppgift2 {
+	public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	
+	String ord = input.next();
+	System.out.println(reverse(ord));		
+ }
+	public static String reverse(String Word) {
+		if(Word.length()==1) {
+			return Word;
+		}else {
+			return Word.charAt(Word.length() - 1) + reverse(Word.substring(0, Word.length() - 1));
+		}
+		
+	}
+}
