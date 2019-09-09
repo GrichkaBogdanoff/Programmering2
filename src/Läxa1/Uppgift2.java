f4057305df35ef5144d25ca4d981930239f65566
GrichkaBogdanoff <42673891+GrichkaBogdanoff@users.noreply.github.com>
2019-09-09 14:26:07 +0200

    Update Uppgift2.java

--- src/Läxa1/Uppgift2.java
+++ src/Läxa1/Uppgift2.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Uppgift2 {
 	public static void main(String[] args) {
-	
-	Scanner input = new Scanner(System.in);
-	
-	String ord = input.next();
-	System.out.println(reverse(ord));		
- }
+
+		Scanner input = new Scanner(System.in);
+
+		String ord = input.next();
+		System.out.println(reverse(ord));
+	}
+
 	public static String reverse(String Word) {
-		if(Word.length()==1) {
+		if (Word.length() == 1) {
 			return Word;
-		}else {
+		} else {
 			return Word.charAt(Word.length() - 1) + reverse(Word.substring(0, Word.length() - 1));
 		}
-		
+
 	}
 }
