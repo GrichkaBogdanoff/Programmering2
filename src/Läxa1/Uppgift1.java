71d0955ed9f66f263ed919839f1709df3a7417f2
GrichkaBogdanoff <42673891+GrichkaBogdanoff@users.noreply.github.com>
2019-09-09 14:26:00 +0200

    Update Uppgift1.java

--- src/Läxa1/Uppgift1.java
+++ src/Läxa1/Uppgift1.java
@@ -2,12 +2,12 @@ package Läxa1;
 
 public class Uppgift1 {
 	public static void main(String[] args) {
-	
-		int[] nummer = new int [] {1,7,6,4,1,2,7};
-		
+
+		int[] nummer = new int[] { 1, 7, 6, 4, 1, 2, 7 };
+
 		System.out.println(summering(nummer));
-}
-	
+	}
+
 	public static int summering(int[] nummer) {
 		int summa = 0;
 		for (int i = 0; i < nummer.length; i++) {
